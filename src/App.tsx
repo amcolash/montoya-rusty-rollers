166ea185a6ef8d3db745595a67dd9e9f108ef1ed
Andrew McOlash <amcolash@gmail.com>
2023-05-20 21:29:15 -0700

    fix build by using hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React, { Suspense } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import { AdminLazy } from './components/LazyComponents';
 import { Website } from './components/website/Website';
 
 export function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
-      path: '/',
+      path: '/*',
       element: <Website />,
     },
     {
       path: '/admin',
       element: (
-        <Suspense fallback="OOP">
+        <Suspense>
           <AdminLazy />
         </Suspense>
       ),
