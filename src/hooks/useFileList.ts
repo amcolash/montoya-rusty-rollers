d91fe5f10a58014129e2b20597f90ea7c07506e9
Andrew McOlash <amcolash@gmail.com>
2023-06-28 23:53:25 -0700

    much faster loading of image list

--- src/hooks/useFileList.ts
+++ src/hooks/useFileList.ts
@@ -1,4 +1,4 @@
-import { getDownloadURL, getStorage, listAll, ref, StorageReference } from 'firebase/storage';
+import { getStorage, listAll, ref, StorageReference } from 'firebase/storage';
 import { useEffect, useState } from 'react';
 import { app } from '../util/firebase';
 
@@ -6,6 +6,7 @@ interface File {
   name: string;
   path: string;
   url: string;
+  thumbnail: string;
   ref: StorageReference;
 }
 
@@ -18,17 +19,18 @@ export function useFileList(path: string, refreshCounter: number): [File[], bool
   useEffect(() => {
     setLoading(true);
 
-    listAll(storageRef).then(async (result) => {
-      const urls = [];
+    listAll(storageRef).then((result) => {
+      const urls: File[] = [];
 
       for (const item of result.items) {
-        const existing = files.find((f) => f.path === item.fullPath);
-        if (existing) {
-          urls.push(existing);
-        } else {
-          const url = await getDownloadURL(item);
-          urls.push({ name: item.name, path: item.fullPath, url, ref: item });
-        }
+        if (item.name.includes('200x200') || item.name.includes('400x400') || item.name.includes('1000x1000')) continue;
+
+        const url = `https://firebasestorage.googleapis.com/v0/b/${item.bucket}/o/${encodeURIComponent(item.fullPath)}?alt=media`;
+
+        const lastDot = url.lastIndexOf('.');
+        const thumbnail = url.includes('.svg') ? url : url.slice(0, lastDot) + '_200x200' + url.slice(lastDot);
+
+        urls.push({ name: item.name, path: item.fullPath, url, thumbnail, ref: item });
       }
 
       setFiles(urls);
