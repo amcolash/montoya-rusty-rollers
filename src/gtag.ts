f0f000f27a8b7e161e5a74f63b748173f1ddb677
Andrew McOlash <amcolash@gmail.com>
2023-08-14 13:05:45 -0700

    Delay google analytics loading

---
+++ src/gtag.ts
@@ -0,0 +1,32 @@
+// Based on https://constantsolutions.dk/2020/06/delay-loading-of-google-analytics-google-tag-manager-script-for-better-pagespeed-score-and-initial-load/
+
+let gtmDidInit = false;
+
+document.addEventListener('DOMContentLoaded', () => setTimeout(initGTM, 3500));
+
+document.addEventListener('scroll', initGTMOnEvent);
+document.addEventListener('mousemove', initGTMOnEvent);
+document.addEventListener('touchstart', initGTMOnEvent);
+
+function initGTMOnEvent(event: Event) {
+  initGTM();
+  event.currentTarget?.removeEventListener(event.type, initGTMOnEvent); // remove the event listener that got triggered
+}
+
+function initGTM() {
+  if (gtmDidInit) {
+    return false;
+  }
+
+  gtmDidInit = true; // flag to ensure script does not get added to DOM more than once.
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+
+  // ensure PageViews is always tracked (on script load)
+  script.onload = () => {
+    window.dataLayer.push({ event: 'gtm.js', 'gtm.start': new Date().getTime(), 'gtm.uniqueEventId': 0 });
+  };
+  script.src = 'https://www.googletagmanager.com/gtm.js?id=G-RWS22TKSLG';
+  document.head.appendChild(script);
+}
